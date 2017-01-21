94f7b11b3ead836c00163640b51d2078cdc7c2e2
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2017-01-20 23:26:19 -0500

    Set up bootstrap; Add button to index.html

--- js/Hello.js
+++ js/Hello.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
-var Hello = React.createClass({
-  render() {
-    return <h1>Hello, world</h1>;
+class Hello extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      sampleProps: 6
+    };
   }
-});
+  render() {
+    return (
+      <div>
+      <h1>Hello, world. Print number {4+6}</h1>
+      <SampleChild sampleProps={this.state.sampleProps}/>
+      </div>
+    )
+  }
+}
+
+class SampleChild extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <a>{this.props.sampleProps}</a>
+    )
+  }
+}
 
 export default Hello;
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import Hello from './Hello';
+import Main from './Main';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<Hello/>, document.getElementById('reactEntry'));
+ReactDOM.render(<Main/>, document.getElementById('reactEntry'));
 
