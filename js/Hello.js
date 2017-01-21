c6ef6a2fbe40ff7e8ca25ed34eb38814ce3c56eb
rtung1333 <gnuttrebor07@gmail.com>
2017-01-20 23:29:11 -0500

    button js

--- js/Hello.js
+++ js/Hello.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 var Hello = React.createClass({
   render() {
-    return <h1>Hello, world</h1>;
+    return ;// <h1>Hello, world</h1>;
   }
 });
+
+function clickButton() {
+
+}
 
 export default Hello;
 
